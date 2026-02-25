544ed462d9f5376530b2571a10b6b39a80737a8f
Tia Ramaya <tiyaya170@gmail.com>
2026-02-25 14:18:40 +0200

    Final Submission

---
+++ src/javaappant2streetsigns/JavaAppant2streetsigns.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaappant2streetsigns;
+
+/**
+ *
+ * @finechinaa
+ */
+public class JavaAppant2streetsigns {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        // The top decorative line
+        System.out.println(""
+         + "\t --------------------");
+        
+        // Drawing the heads of the signs 
+        System.out.println("\t  *****      XXXX");
+        System.out.println("\t *     *    X    X");
+        System.out.println("\t* STOP  *  X  GO  X");
+        System.out.println("\t *     *    X    X");
+        System.out.println("\t  *****      XXXX");
+        
+        // Drawing the stems of the signs
+        System.out.println("\t   **         XX");
+        System.out.println("\t   **         XX");
+        System.out.println("\t   **         XX");
+        System.out.println("\t   **         XX");
+        System.out.println("\t  *****      XXXX");
+        
+        // Drawing the bases
+        System.out.println("\t********** XXXXXXXXXX");
+        
+        // The bottom ground lines using escape sequences for spacing
+        System.out.println("|----------------------------------|");
+        System.out.println("|----------------------------------|");
+        
+        System.out.println("\n\t\tSTOP & GO signs");     
+    }   
+}
